027a974ab3d34bb5d845ccf501dbbe10f2aac2ac
Purushottam-Malbishoye <125381416+Purushottam-Malbishoye@users.noreply.github.com>
2025-07-25 03:52:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h1>AWS CodePipeline using Terraform</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
